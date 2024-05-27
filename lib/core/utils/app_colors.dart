b668af29b6ae2aa2ae89138bdf28d04fc2f7296c
Myproject <mohamedAlaa-CS>
2024-05-27 16:21:49 +0300

    add app color file

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,13 @@
+abstract class AppColors {
+  static const String white = "0xffffffff";
+  static const String black = "0xff000000";
+
+  static const String primaryColor = "0xff080020";
+  static const String violateColor = "0xff140D2B";
+  static const String ligthColor = "0xffCECCD2";
+  static const String borderColor = "0xff5E596E";
+  static const String redColor = "0xffDF2323";
+  static const String ligthredColor = "0xffFF0000";
+  static const String yellowColor = "0xffFFC700";
+  static const String green = "0xff05F300";
+}
