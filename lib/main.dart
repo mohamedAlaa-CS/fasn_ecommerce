002ed643b8689ea3abddc188b941f1db946a1b8a
Myproject <mohamedAlaa-CS>
2024-05-27 14:39:32 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:fasn_ecommerce/generated/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: const Locale('en'),
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+       
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      home: const TestLocal(),
+    );
+  }
+}
+
+class TestLocal extends StatelessWidget {
+  const TestLocal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(S.of(context).title),
+      ),
+    );
+  }
+}
