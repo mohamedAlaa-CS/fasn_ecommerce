3777ad49aa97737ac448fc328251d9361d21d602
Myproject <mohamedAlaa-CS>
2024-06-04 15:32:59 +0300

    refactor cart ui

--- lib/features/cart/presentaion/views/cart_view.dart
+++ lib/features/cart/presentaion/views/cart_view.dart
@@ -3,6 +3,7 @@ import 'package:fasn_ecommerce/features/cart/presentaion/views/widgets/cart_prod
 import 'package:fasn_ecommerce/features/cart/presentaion/views/widgets/checkout_button.dart';
 import 'package:fasn_ecommerce/features/cart/presentaion/views/widgets/total_price_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CartView extends StatelessWidget {
   const CartView({super.key});
@@ -12,27 +13,37 @@ class CartView extends StatelessWidget {
     return Column(
       children: [
         Expanded(
-          child: ListView.separated(
-            separatorBuilder: (context, index) => 10.hSize,
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return CartProductWidget(
-                imageUrl:
-                    'https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
-                title: 'Product Title',
-                price: 100,
-                oldPrice: 120,
-                qty: 2,
-                addItem: () {},
-                removeItem: () {},
-                deleteItem: () {},
-              );
-            },
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                ListView.separated(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  separatorBuilder: (context, index) => 10.hSize,
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return CartProductWidget(
+                      imageUrl:
+                          'https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
+                      title: 'Product Title',
+                      price: 100,
+                      oldPrice: 120,
+                      qty: 2,
+                      addItem: () {},
+                      removeItem: () {},
+                      deleteItem: () {},
+                    );
+                  },
+                ),
+                15.hSize,
+                const TotalWidget(
+                  totalPrice: 200,
+                ),
+                10.hSize,
+              ],
+            ),
           ),
-        ),
-        3.hSize,
-        const TotalWidget(
-          totalPrice: 200,
         ),
         3.hSize,
         ChechoutButton(
